b11e9ef697d8e2fc41ca2ce1170fa3c06c2ccf41
Lenovo <benasvol@gmail.com>
2022-06-27 22:06:03 +0300

    Improve formatting and add verify function for programmatic verification

--- contracts/MarketSentiments.sol
+++ contracts/MarketSentiments.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity 0.8.7;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -15,16 +15,16 @@ contract MarketSentiments is Ownable {
     string[] public tickersArray;
     mapping(string => Ticker) public tickers;
 
-    event tickerAdded(string ticker, address cryptoAddress);
+    event TickerAdded(string ticker, address cryptoAddress);
 
-    event tickerUpdated(
+    event TickerUpdated(
         uint256 upCount,
         uint256 downCount,
         address voter,
         string ticker
     );
 
-    function addTicker(string calldata _ticker, address _cryptoAddress)
+    function addTicker(string memory _ticker, address _cryptoAddress)
         public
         onlyOwner
     {
@@ -33,11 +33,10 @@ contract MarketSentiments is Ownable {
         newTicker.exists = true;
         newTicker.cryptoAddress = _cryptoAddress;
         tickersArray.push(_ticker);
-
-        emit tickerAdded(_ticker, _cryptoAddress);
+        emit TickerAdded(_ticker, _cryptoAddress);
     }
 
-    function vote(string calldata _ticker, bool _voteIsUp) public {
+    function vote(string memory _ticker, bool _voteIsUp) public {
         require(tickers[_ticker].exists, "This token does not exist.");
         require(
             tickers[_ticker].voters[msg.sender] == false,
@@ -52,11 +51,10 @@ contract MarketSentiments is Ownable {
         } else {
             t.downCount++;
         }
-
-        emit tickerUpdated(t.upCount, t.downCount, msg.sender, _ticker);
+        emit TickerUpdated(t.upCount, t.downCount, msg.sender, _ticker);
     }
 
-    function getVotes(string calldata _ticker)
+    function getVotes(string memory _ticker)
         public
         view
         returns (uint256 up, uint256 down)
--- scripts/deploy-market-sentiments.js
+++ scripts/deploy-market-sentiments.js
@@ -8,7 +8,28 @@ async function main() {
     await marketSentiments.deployed();
 
     console.log("MarketSentiments deployed to:", marketSentiments.address);
+    if (hre.network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for block confirmations...");
+        await simpleStorage.deployTransaction.wait(6);
+        await verify(simpleStorage.address, []);
+    }
 }
+
+const verify = async (contractAddress, args) => {
+    console.log("Verifying contract...");
+    try {
+        await hre.run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        });
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!");
+        } else {
+            console.log(e);
+        }
+    }
+};
 
 main()
     .then(() => process.exit(0))
